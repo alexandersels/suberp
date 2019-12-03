bc65b7c0884a3f606f2ef9df94550782cf4517bf
Alexander Sels <alexandersels@hotmail.com>
2019-12-03 21:41:14 +0100

    feature: add default font to site.

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DesktopNavigationHeaderComponent} from "./desktop-navigation-header/desktop-navigation-header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesktopNavigationHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ website/src/app/desktop-navigation-header/desktop-navigation-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-navigation-header',
+  templateUrl: './desktop-navigation-header.component.html',
+  styleUrls: ['./desktop-navigation-header.component.scss']
+})
+export class DesktopNavigationHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
