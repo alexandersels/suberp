1939027b767c2f410d4f6be6b7ec9c8ae1dc0be9
sachaGit <sachawuyts@gmail.com>
2019-12-03 22:30:13 +0100

    feature/ added-footer

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {DesktopNavigationHeaderComponent} from "./desktop-navigation-header/desktop-navigation-header.component";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DesktopNavigationHeaderComponent
+    DesktopNavigationHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
